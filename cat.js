7848e7401293814fdc4d0714baf414bc2dc462af
nguyenvantinh <tinhtienty1997@gmail.com>
2019-08-27 11:46:49 +0700

    'goo'

--- cat.js
+++ cat.js
@@ -1,3 +1,13 @@
-function Cat() {
+var chalk = require('chalk');
+
+function Cat(name) {
+	this.Name = name;
+	this.Stomach = [];
 }
-Module.exports = Cat;
+Cat.prototype.eat = function(mouse) {
+	this.Stomach.push(mouse);
+};
+Cat.prototype.sayHi = function() {
+	console.log('Hi i am a cat, my name is '+ chalk.blue(this.Name));
+};
+module.exports = Cat;
---
+++ index.js
@@ -0,0 +1,4 @@
+var Cat = require('./cat');
+
+var cat = new Cat('Tom');
+cat.sayHi();
